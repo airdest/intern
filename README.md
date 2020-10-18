a6edd377a8e3fe9716f9a4d1c25c63c0479c10b1
Airdest <airdest@163.com>
2020-10-18 21:04:38 +0800

    init project

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 <br/>
 # 技术点
 编程语言：Java11<br/>
-项目管理工具Maven3.6.3<br/>
+项目管理工具：Maven3.6.3<br/>
 开发工具：Idea or Eclipse<br/>
-项目框架：SpringCloud<br/>
+项目框架：SpringCloud + Eureka<br/>
---
+++ src/main/java/com/chuxingiao/intern/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.chuxingiao.intern;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : airde
+ * @date : 2020/10/18 18:40
+ * @since :
+ */
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+
+    }
+}
